948f98ec6a54068575360588833db020c704f17b
rduret <remi.duret.dev@gmail.com>
2023-08-08 16:20:19 +0200

    Mise en place édit d'un modèle + fix upload

--- src/Controller/Calendar/ModelCalendarController.php
+++ src/Controller/Calendar/ModelCalendarController.php
@@ -3,14 +3,15 @@
 namespace App\Controller\Calendar;
 
 use Symfony\Component\Uid\Uuid;
+use App\Service\Utils\UploadHandler;
 use App\Entity\Calendar\ModelCalendar;
 use App\Form\Calendar\ModelCalendarType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\Calendar\ModelCalendarRepository;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 
@@ -25,7 +26,7 @@ class ModelCalendarController extends AbstractController
     }
 
     #[Route('/admin/calendriers/new', name: 'model_calendar_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ModelCalendarRepository $modelCalendarRepository, SluggerInterface $slugger): Response
+    public function new(Request $request, ModelCalendarRepository $modelCalendarRepository, UploadHandler $uploadHandler): Response
     {
         $modelCalendar = new ModelCalendar();
         $form = $this->createForm(ModelCalendarType::class, $modelCalendar);
@@ -35,17 +36,9 @@ class ModelCalendarController extends AbstractController
             /** @var UploadedFile $file */
             $file = $form->get('file')->getData();
             if ($file !== null) {
-                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
-
-                // Move the file to the directory where brochures are stored
                 try {
-                    $file->move(
-                        $this->getParameter('files_directory'),
-                        $newFilename
-                    );
+                    $newFilename = $uploadHandler->uploadFile($file, $request->get('cropped-file'));
+                    $modelCalendar->setPath($newFilename);
                 } catch (FileException $e) {
                     $this->addFlash(
                         'error',
@@ -57,7 +50,6 @@ class ModelCalendarController extends AbstractController
                         'form' => $form,
                     ]);
                 }
-                $modelCalendar->setPath($newFilename);
             }
 
             $modelCalendar->setUser($this->getUser());
@@ -90,18 +82,41 @@ class ModelCalendarController extends AbstractController
     }
 
     #[Route('/admin/calendriers/{id}/edit', name: 'model_calendar_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, ModelCalendar $modelCalendar, ModelCalendarRepository $modelCalendarRepository): Response
+    public function edit(Request $request, ModelCalendar $modelCalendar, ModelCalendarRepository $modelCalendarRepository, UploadHandler $uploadHandler): Response
     {
         $form = $this->createForm(ModelCalendarType::class, $modelCalendar);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('file')->getData();
+            if ($file !== null) {
+                try {
+                    $newFilename = $uploadHandler->uploadFile($file, $request->get('cropped-file'));
+                    $modelCalendar->setPath($newFilename);
+                } catch (FileException $e) {
+                    $this->addFlash(
+                        'error',
+                        'Une erreur s\'est produite lors de l\'upload'
+                    );
+
+                    return $this->render('calendar/model_calendar/edit.html.twig', [
+                        'model' => $modelCalendar,
+                        'form' => $form,
+                    ]);
+                }
+            }
+
             $modelCalendarRepository->save($modelCalendar, true);
+
+            $this->addFlash(
+                'successs',
+                'Le modèle a bien été édité'
+            );
 
             return $this->redirectToRoute('model_calendar_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('calendar/model_calendar/edit.html.twig', [
+        return $this->render('calendar/model_calendar/edit.html.twig', [
             'model' => $modelCalendar,
             'form' => $form,
         ]);
@@ -110,7 +125,8 @@ class ModelCalendarController extends AbstractController
     #[Route('/admin/calendriers/{id}', name: 'model_calendar_delete', methods: ['POST'])]
     public function delete(Request $request, ModelCalendar $modelCalendar, ModelCalendarRepository $modelCalendarRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$modelCalendar->getId(), $request->request->get('_token'))) {
+
+        if ($this->isCsrfTokenValid('delete_'.$modelCalendar->getId(), $request->request->get('_token'))) {
             $modelCalendarRepository->remove($modelCalendar, true);
         }
 
