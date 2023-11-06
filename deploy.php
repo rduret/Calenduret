3ad84ab18ece86e03899d0feb9abd7f9d19f9b9c
rduret <remi.duret.dev@gmail.com>
2023-11-06 14:14:20 +0100

    deploy.php

---
+++ deploy.php
@@ -0,0 +1,28 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+
+set('repository', 'git@github.com:rduret/advent-calendars.git');
+//set('http_user', 'www-data');
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+host('production')
+    ->set('hostname', '62.72.16.63')
+    ->set('remote_user', 'rduret')
+    ->set('deploy_path', '~/htdocs/calenduret.fr');
+
+// Hooks
+task('build', function () {
+    cd('{{release_path}}');
+    run('npm install');
+    run('npm run build');
+});
+
+after('deploy:update_code', 'build');
+after('deploy:failed', 'deploy:unlock');
